b27d2357d8ffce8d9a8ea51d8ce99e87620a93d9
Sowmya020 <tangisowmya@gmail.com>
2025-03-31 18:24:54 +0530

    Update and rename Notifications.js to Notifications.jsx

--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -3,7 +3,6 @@ import '../assets/styles/Notifications.css';
 
 const Notifications = () => (
     <div className="notifications">
-        <h2>Notifications</h2>
         <ul>
             <li>LH Bus is delayed by 10 mins</li>
             <li>Reminder: MHB will be arriving in 5 minutes at MHB</li>
