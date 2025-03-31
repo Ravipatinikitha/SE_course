c5d595ffdcf9cba8cd5c0b5572eff8b040f95783
Sowmya020 <tangisowmya@gmail.com>
2025-03-31 19:00:02 +0530

    Add files via upload

---
+++ frontend/src/pages/Buscard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/BusCard.css';
+
+const BusCard = ({ bus }) => (
+    <div className="bus-card">
+        <h3>{bus.name}</h3>
+        <p>Departure: {bus.departure} - {bus.time}</p>
+        <p>Arrival: {bus.arrival}</p>
+        <button>View in Map</button>
+    </div>
+);
+
+export default BusCard;
