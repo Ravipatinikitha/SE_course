0a29b7e853b92f102d4c6cea4cf1bc23fdc53918
Sowmya020 <tangisowmya@gmail.com>
2025-03-31 18:23:57 +0530

    Update and rename MapPage.js to MapPage.jsx

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -4,7 +4,6 @@ import '../assets/styles/MapPage.css';
 function MapPage() {
     return (
         <div className="map-page">
-            <h1>Map Page</h1>
             <p>View bus routes and stops here.</p>
             <div className="map-placeholder">
                 📍 Map will be displayed here.
